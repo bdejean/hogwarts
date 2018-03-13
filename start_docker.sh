b1f7d90d30601d1aed7552b905effd3f8980141d
Benoît Dejean <bdejean@gmail.com>
2018-03-13 07:20:31 +0100

    To use a sqlite db file, it's important to share/mount the whole directory so that lock and journal files are also visible. Mount the whole db/ dir.

--- start_docker.sh
+++ start_docker.sh
@@ -12,7 +12,8 @@ APACHE_SERVERNAME=hogwarts.lan
 APP_ENV=local # TODO Change
 APP_DEBUG=true # TODO Change
 APP_KEY=
-DB_DATABASE=
+DB_DIR=
+DB_FILE=hogwarts.db
 
 
 while [[ ! -z $1 ]]; do
@@ -21,20 +22,24 @@ while [[ ! -z $1 ]]; do
 	    BUILD_IMAGE=true
 	    ;;
 	*)
-	    DB_DATABASE="$1"
+	    DB_DIR="$1"
 	    ;;
     esac
     shift
 done
 
-if [[ -z $DB_DATABASE ]]; then
-    DB_DATABASE=hogwarts.db
-    DB_DATABASE=$(realpath $DB_DATABASE)
-    if [ ! -f $DB_DATABASE ]; then
-	echo "$DB_DATABASE doest not exist, create and seed it!"
+if [[ -z $DB_DIR ]]; then
+    DB_DIR=db
+    DB_DIR=$(realpath $DB_DIR)
+    if [ ! -d $DB_DIR ]; then
+	echo "$DB_DIR doest not exist, create it!"
 	exit 1
     fi
-    echo "Default db file to $DB_DATABASE"
+    if [ ! -f $DB_DIR/$DB_FILE ]; then
+	echo "$DB_FILE doest not exist, create and seed it!"
+	exit 1
+    fi
+    echo "Default db dir to $DB_DIR"
 fi
 
 if [[ -z $(docker images -q $IMAGE_NAME 2> /dev/null) || $BUILD_IMAGE ]]; then
@@ -44,12 +49,12 @@ fi
 
 docker run -i -d -p 44080:80 \
        -v /etc/localtime:/etc/localtime:ro \
-       -v "$DB_DATABASE:/var/lib/db/hogwarts.db" \
+       -v "$DB_DIR:/var/lib/db" \
        -e APACHE_SERVERNAME="$APACHE_SERVERNAME" \
        -e APP_ENV="$APP_ENV" \
        -e APP_DEBUG="$APP_DEBUG" \
        -e APP_KEY="$APP_KEY" \
-       -e DB_DATABASE=/var/lib/db/hogwarts.db \
+       -e DB_DATABASE=/var/lib/db/$DB_FILE \
        -e SEED_DATABASE="$SEED_DATABASE" \
        -e ADMIN_PASSWORD=secret \
        "$IMAGE_NAME"
