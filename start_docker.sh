9eef50bcb1b32093abb3da169bc078150fad6835
Benoît Dejean <bdejean@gmail.com>
2018-03-13 07:14:59 +0100

    Drop old mysql start code.

--- start_docker.sh
+++ start_docker.sh
@@ -41,18 +41,6 @@ if [[ -z $(docker images -q $IMAGE_NAME 2> /dev/null) || $BUILD_IMAGE ]]; then
     docker build -t "$IMAGE_NAME" . || exit 1
 fi
 
-# Assume that mysql and hogwarts will run on the same host
-#DB_CONTAINER=`docker run -d -p 127.0.0.1:$DB_PORT:3306 \
-#       -v "$DB_DATABASE:/var/lib/mysql" \
-#       -e MYSQL_RANDOM_ROOT_PASSWORD=yes \
-#       -e MYSQL_ONETIME_PASSWORD=yes \
-#       -e MYSQL_DATABASE="$DB_DATABASE" \
-#       -e MYSQL_USER="$DB_USERNAME" \
-#       -e MYSQL_PASSWORD="$DB_PASSWORD" \
-#       mysql`
-#echo $DB_CONTAINER
-#
-#DB_HOST=`docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $DB_CONTAINER`
 
 docker run -i -d -p 44080:80 \
        -v /etc/localtime:/etc/localtime:ro \
