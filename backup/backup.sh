a62519f98f69d892c310d150d7f2ccaabb186e3b
Benoît Dejean <bdejean@gmail.com>
2018-03-18 10:37:25 +0000

    Improve backup script to do a dump and a .backup command.

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,9 +1,42 @@
 #!/bin/bash
 
+
+function exists_or_die {
+	P=$1
+	if [[ ! -e "$P" ]]; then
+		echo "File or directory '$P' does not exists"
+		exit 1
+	fi
+}
+
+
+function doesnt_exist_or_die {
+	P=$1
+	if [[ -e "$P" ]]; then
+		echo "File or directory '$P' already exists"
+		exit 2
+	fi
+}
+
+
 DB=$1
 BACKUP_DIR=$2
 
 TIMESTAMP=$(date +"%Y-%m-%d-%HH%M")
 
-cp -nvp $DB ${BACKUP_DIR}/${DB##*/}.${TIMESTAMP}
+DEST_BASE=${BACKUP_DIR}/${DB##*/}.${TIMESTAMP}
+DEST_DUMP=${DEST_BASE}.dump
+DEST_BCK=${DEST_BASE}.bck
+
+exists_or_die $DB
+exists_or_die $BACKUP_DIR
+doesnt_exist_or_die $DEST_DUMP
+doesnt_exist_or_die $DEST_BCK
+doesnt_exist_or_die $DEST_DUMP.gz
+doesnt_exist_or_die $DEST_BCK.gz
+
+
+sqlite3 $DB '.dump' > $DEST_DUMP
+sqlite3 $DB ".backup $DEST_BCK"
+gzip -9 $DEST_DUMP $DEST_BCK
 
